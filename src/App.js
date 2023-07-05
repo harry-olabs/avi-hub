695b4890fe5b9bac34bc1479856f29e78b6619b9
Adedoyin Tioluwanimi <adedoyintioluwanimi@gmail.com>
2023-07-05 11:56:00 +0100

    created an empty components for the registration page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from "react";
 import AppRoutes from "./components/Routes";
 
 function App() {
+<<<<<<< HEAD
   return <AppRoutes />;
+=======
+  return (
+    <div className="App">
+      {/* <Login /> */}
+      <Registration/>
+    </div>
+  );
+>>>>>>> 050cfaa (created an empty components for the registration page)
 }
 export default App;
