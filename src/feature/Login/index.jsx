cd8d5a1c88b0893315ee57ea61ab003bea03ccdd
Adedoyin Tioluwanimi <adedoyintioluwanimi@gmail.com>
2023-07-06 17:28:10 +0100

    removed the routes link that was not in use

--- src/feature/Login/index.jsx
+++ src/feature/Login/index.jsx
@@ -4,7 +4,6 @@ import styles from "./Login.module.css";
 import logo from "../../images/sniper.png";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
-import { Routes } from "react-router-dom";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
