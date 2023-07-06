79e548134c24d1efd4eb321797d6a2462600f071
Adedoyin Tioluwanimi <adedoyintioluwanimi@gmail.com>
2023-07-06 16:27:17 +0100

    added the registration page

--- src/feature/Registration/index.jsx
+++ src/feature/Registration/index.jsx
@@ -1,8 +1,80 @@
 import React from "react";
 import styles from "./Registration.module.css";
+import logo from "../../images/sniper.png";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import { useState } from "react";
 
 const Registration = () => {
-  return <div className={styles.color}>This is the Registration Page</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeat, setRepeat] = useState("");
+
+  const handleEmailChange = (value) => {
+    setEmail(value);
+  };
+
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+  };
+  const handleRepeatPasswordChange = (value) => {
+    setRepeat(value);
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.container__logo}>
+        <img src={logo} alt="Description" />
+      </div>
+      <h1 className={styles.container__title}>Register</h1>
+      <p className={styles.container__description}>Access to our dashboard</p>
+      <form className={styles.form}>
+        <div className={styles.form__group}>
+          <label for="email" className={styles.form__label}>
+            Email Address:
+          </label>
+          <Input
+            type="email"
+            placeholder="Enter your Email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div className={styles.form__group}>
+          <label for="password" className={styles.form__label}>
+            Password:
+          </label>
+          <Input
+            type="password"
+            placeholder="Enter password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div className={styles.form__group}>
+          <label for="repeat-password" class={styles.form__label}>
+            Repeat Password:
+          </label>
+          <Input
+            type="repeat-password"
+            placeholder="Repeat-password"
+            value={repeat}
+            onChange={handleRepeatPasswordChange}
+          />
+        </div>
+        <div className="form__group">
+          <Button text="register" />
+        </div>
+      </form>
+      <div className={styles.register__link}>
+        <p>
+          Already have an account?
+          <a href="dashboard.html"className={styles.register__link__anchor}>
+            Login
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Registration;
