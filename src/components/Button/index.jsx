8354ab7b1b235f5ccdd58cef04a6c0fc99a62cdf
Adedoyin Tioluwanimi <adedoyintioluwanimi@gmail.com>
2023-07-19 12:18:12 +0100

    rebased the login branch

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import "./Button.css";
 
-<<<<<<< HEAD
 const Button = ({text, handleClick }) => {
-=======
-const Button = ({ text, handleClick }) => {
->>>>>>> 1d379478d5f90bcd2cc22b59b41c0e18e8135eaa
   return (
       <button className="custom-btn" onClick={handleClick}>{text}</button>
   );
