a63473b472ee82158cee23d1b3567fe17019df00
Doyin02 <80858126+Doyin02@users.noreply.github.com>
2023-09-27 07:19:50 +0100

    AVI-40: implement login functionality
    Closes #40

--- src/feature/Login/index.jsx
+++ src/feature/Login/index.jsx
@@ -1,37 +1,131 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../../redux/actions/auth-actions";
 import styles from "./Login.module.css";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../images/sniper.png";
 import Button from "../../components/Button";
-import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/Input";
+import Cookies from "universal-cookie";
+import useFetch from "../../hooks/use-fetch";
 import { validateEmail, validatePassword } from "../../utils/validation";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData;
+
   const [emailValid, setEmailValid] = useState(true);
   const [passwordValid, setPasswordValid] = useState(true);
+  const [loginFailed, setLoginFailed] = useState(false);
+  const [isEmptyInput, setIsEmptyInput] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [dots, setDots] = useState("");
+
+  const { data, error, makeRequest } = useFetch(
+    "https://afrihub-backend.onrender.com/api/auth/login",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  useEffect(() => {
+    if (data) {
+      const { accessToken } = data;
+
+      const cookies = new Cookies();
+      cookies.set("accessToken", accessToken, { path: "/" });
+
+      // Decode the token to obtain the registered user details
+      const decodedData = jwt_decode(accessToken);
+
+      dispatch(loginSuccess(decodedData));
+
+      navigate("/dashboard");
+    }
+  }, [data, dispatch, navigate]);
+
+  useEffect(() => {
+    if (error) {
+      setLoginFailed(true);
+      const timeoutId = setTimeout(() => {
+        setLoginFailed(false);
+      }, 3000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+
+    if (isEmptyInput) {
+      const timeoutId = setTimeout(() => {
+        setIsEmptyInput(false);
+      }, 3000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+  }, [error, isEmptyInput]);
+
+  useEffect(() => {
+    let dotsInterval;
+
+    if (isLoading) {
+      dotsInterval = setInterval(() => {
+        setDots((prevDots) => (prevDots === "..." ? "" : prevDots + "."));
+      }, 500);
+    } else {
+      clearInterval(dotsInterval);
+      setDots("");
+    }
+
+    return () => clearInterval(dotsInterval);
+  }, [isLoading]);
 
   const handleEmailChange = (value) => {
-    setEmail(value);
+    setFormData((prevState) => ({
+      ...prevState,
+      email: value,
+    }));
     setEmailValid(validateEmail(value));
   };
 
   const handlePasswordChange = (value) => {
-    setPassword(value);
+    setFormData((prevState) => ({
+      ...prevState,
+      password: value,
+    }));
     setPasswordValid(validatePassword(value));
   };
 
-  const handleClick = () => {
-    if (!validateEmail(email) || !passwordValid || password.length < 6) {
-      alert("Please enter a valid email and password (at least 6 characters long).");
+  const handleLoginEvent = async (e) => {
+    e.preventDefault();
+
+    if (!email || !password) {
+      setIsEmptyInput(true);
       return;
     }
 
-    // Additional login logic here, e.g., sending data to the server for authentication.
+    if (!emailValid || !passwordValid) {
+      setLoginFailed(true);
+      return;
+    }
 
-    navigate("/dashboard");
+    setIsLoading(true);
+
+    await makeRequest({
+      email,
+      password,
+    });
   };
 
   return (
@@ -41,16 +135,26 @@ const Login = () => {
           <img src={logo} alt="Description" />
         </div>
         <h1 className={styles.container__title}>Login</h1>
-        <form className={styles.form}>
+        {loginFailed && (
+          <div className="alert alert-danger" role="alert">
+            Login failed. Please check your credentials and try again.
+          </div>
+        )}
+        {isEmptyInput && (
+          <div className="alert alert-danger" role="alert">
+            Input fields cannot be empty.
+          </div>
+        )}
+        <form className={styles.form} onSubmit={handleLoginEvent}>
           <div className={styles.form__group}>
             <label htmlFor="email" className={styles.form__label}>
               Email Address:
             </label>
             <Input
               type="email"
+              placeholder="Enter your Email"
               value={email}
               onChange={handleEmailChange}
-              placeholder="Enter your email"
               isValid={emailValid}
               errorMessage="Invalid email format."
             />
@@ -61,18 +165,24 @@ const Login = () => {
             </label>
             <Input
               type="password"
+              placeholder="Enter your password"
               value={password}
               onChange={handlePasswordChange}
-              placeholder="Enter your password"
-              isValid={passwordValid && password.length >= 6}
+              isValid={passwordValid}
               errorMessage="Password must be at least 6 characters long."
             />
           </div>
-          <Button text="Login" handleClick={handleClick} />
+          <div className="form__group">
+            <Button
+              text={`${isLoading ? "Logging in" : "Login"}${dots}`}
+              handleClick={handleLoginEvent}
+              disabled={isLoading}
+            />
+          </div>
         </form>
         <div className={styles.login__link}>
           <p>
-            Don't have an account yet?
+            Don't have an account?
             <span>
               <Link to="/register" className={styles.login__link__anchor}>
                 Register
--- src/redux/actions/auth-actions.js
+++ src/redux/actions/auth-actions.js
@@ -1,6 +1,10 @@
-import { REGISTRATION_SUCCESS } from "../types/action-types";
+import { REGISTRATION_SUCCESS, LOGIN_SUCCESS  } from "../types/action-types";
 
 export const registrationSuccess = (userCredentials) => ({
   type: REGISTRATION_SUCCESS,
   payload: userCredentials,
+});
+
+export const loginSuccess = () => ({
+  type: LOGIN_SUCCESS,
 });
